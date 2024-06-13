2f3e9cab55ed8ed5efd3d1e2ab24f34e952d2381
Emily Morgan <emorgan921@gmail.com>
2024-06-13 16:32:59 -0400

    Add project files.

---
+++ DictionaryPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DictionaryPractice;
+
+internal class Program {
+    static void Main(string[] args) {
+        List<Student> students = new List<Student>();
+        students.Add(new Student { Id = 1,Name = "Fred" });
+        students.Add(new Student { Id = 2,Name = "Wilma" });
+        students.Add(new Student { Id = 3,Name = "Emily" });
+
+        Dictionary<int,Student> StudentDictionary = new Dictionary<int,Student>();
+        foreach(var student in students) {
+            StudentDictionary.Add(student.Id,student);
+        }
+        var Wilma = StudentDictionary[2];
+        Console.WriteLine($"Id: {Wilma.Id}, Name: {Wilma.Name}");
+        /*Dictionary<int,string> dictionary = new Dictionary<int,string>();
+        dictionary.Add(1,"one");
+        dictionary.Add(22,"twenty-two");
+        dictionary.Add(14,"Fourteen");*/
+    }
+}
+internal class Student {
+    public int Id {
+        get; set;
+    }
+    public string Name {
+        get; set;
+    }
+}
